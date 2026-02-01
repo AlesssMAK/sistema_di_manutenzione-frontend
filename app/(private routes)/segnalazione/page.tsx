b55ef69a617284d77f2304ea646309348d9046c5
Alexey <makovii88@gmail.com>
2026-02-01 12:27:21 +0100

    update css for header

--- app/(private routes)/segnalazione/page.tsx
+++ app/(private routes)/segnalazione/page.tsx
@@ -1,5 +1,12 @@
 const SegnalazionePage = () => {
-  return <div>SegnalazionePage</div>;
+  return (
+    <main>
+      <div className="container">
+        <h1>Nuova Segnalazione</h1>
+        <p>Compila il modulo per segnalare un guasto o anomalia</p>
+      </div>
+    </main>
+  );
 };
 
 export default SegnalazionePage;
