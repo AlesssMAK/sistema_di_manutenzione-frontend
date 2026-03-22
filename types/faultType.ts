51ebdb5d80be315beadef3221f14bb1f662bf6c9
Iryna-M-html <ivmohnataya@gmail.com>
2026-03-22 20:02:10 +0200

    create interface FaultCard

---
+++ types/faultType.ts
@@ -0,0 +1,28 @@
+export type StatusFaultType = 'Creato' | 'In corso' | 'Completato' | 'Sospeso';
+export type PriorityFaultType = 'Bassa' | 'Media' | 'Alta';
+export interface FaultCard {
+  id: string;
+  faultId: string;
+
+  plantId: {
+    _id: string;
+    namePlant: string;
+    code: string;
+  };
+  partId: {
+    namePartPlant: string;
+    codePartPlant: string;
+  };
+  nameOperator: string;
+  userId: string;
+
+  statusfault: StatusFaultType;
+  comment: string;
+  priority: PriorityFaultType;
+  deadline?: string;
+  plannedDate?: string;
+  plannedTime?: string;
+  estimatedDuration?: number;
+  img?: string;
+  managerComment?: string;
+}
