1a6492544b0468da78857e3718855fb996845e67
Iryna-M-html <ivmohnataya@gmail.com>
2026-03-23 14:34:28 +0200

    fixed questions

--- app/(private routes)/maintenance-worker/page.tsx
+++ app/(private routes)/maintenance-worker/page.tsx
@@ -1,23 +1,9 @@
-import FaultCardsList from '@/components/FaultCardsList/FaultCardsList';
 import MaintenanceWorkerClient from './MaintenanceWorker.client';
 
-import CalendarBlock from '@/components/CalendarBlock/CalendarBlock';
-import css from './page.module.css';
-import { fetchFaultCards } from '@/lib/api/faults';
-
-const maintenanceWorkerPage = async () => {
-  const { items } = await fetchFaultCards({ page: 1, limit: 2 });
+const maintenanceWorkerPage = () => {
   return (
     <div>
       <MaintenanceWorkerClient />
-      <h2 className={css.workerHeaderPage}>Pianificazione Manutentore</h2>
-      <p className={css.workerTextPage}>
-        Visualizza e gestisci gli interventi pianificati
-      </p>
-      <div className={css.workerContainer}>
-        <CalendarBlock />
-        <FaultCardsList faults={items} />
-      </div>
     </div>
   );
 };
